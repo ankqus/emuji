61fbd22623932473f885f5bea3aa3074048b577e
Ankush <ankush.th1470@gmail.com>
2023-04-23 01:08:03 +0545

    emuji to emoji

--- txt_to_json.py
+++ txt_to_json.py
@@ -3,33 +3,14 @@ import os
 import re
 import requests
 import time
-# import shutil
-# import threading
 
 version = "15.0"
 
-# stop_dancing_text = False
-
-# def dancing_text(text):
-# global stop_dancing_text
-# while not stop_dancing_text:
-# for i in range(len(text)):
-# if stop_dancing_text:
-# break
-# columns, rows = shutil.get_terminal_size()
-# center = (columns - len(text)) // 2
-# print(
-#   "\033[s" + " " * center + text[:i] + text[i].upper() +
-#   text[i + 1:].lower() + "\033[u",
-#   end="\r",
-#   flush=True,
-# )
-# time.sleep(0.5)
-# print("\033[s" + "Fetching complete." + "\033[u")
-
-
 def main():
+    start = time.time()
     text = get_test_file(version)
+    end = time.time()
+    time_taken = end - start
     print("[FETCHED]")
     print()
     # global stop_dancing_text
@@ -45,8 +26,6 @@ def main():
             accu["group"] = line[9:]
         elif line.startswith("# subgroup: "):
             accu["sub-group"] = line[12:]
-        # elif line.startswith("#"):
-        # accu["comments"] += line + "\n"
         else:
             meta = parse_line(line)
             if meta:
@@ -57,14 +36,20 @@ def main():
                 accu["compact"].append(meta["char"])
             else:
                 accu["comments"] = accu["comments"].strip() + "\n\n"
-    print(f"\nProcessed emujis: {len(accu['full'])}")
+    print(f"\nProcessed emojis: {len(accu['full'])}")
     print(f"Version: (v{version})")
-    print("O/P file: emuji.json, emuji-only.json\n")
+    print("O/P file: emoji.json, emoji-only.json")
+    print(f"Total time taken: {time_taken:.2f} seconds\n")
     write_files(accu)
 
-
-def print_progress_bar(
-    iteration, total, prefix="", suffix="", decimals=1, length=50, fill="█"
+def pro_bar(
+    iteration, 
+    total, 
+    prefix="", 
+    suffix="", 
+    decimals=1, 
+    length=50, 
+    fill="█"
 ):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
@@ -72,7 +57,6 @@ def print_progress_bar(
     print(f"\r{prefix} |{bar}| {percent}% {suffix}", end="\r")
     if iteration == total:
         print()
-
 
 def get_test_file(ver):
     url = f"https://unicode.org/Public/emoji/{ver}/emoji-test.txt"
@@ -83,18 +67,15 @@ def get_test_file(ver):
     ), f"Request failed with status {response.status_code}"
 
     text = response.text
-
     # dancing_text_thread = threading.Thread(target=dancing_text,
     # args=("Formatting Text to Json", ))
-
     # dancing_text_thread.daemon = True
     # dancing_text_thread.start()
-
+    print()
     for i in range(61):
-        print_progress_bar(i, 60)
+        pro_bar(i, 60)
         time.sleep(0.2)
     return text
-
 
 def parse_line(line):
     data = re.split(r"\s+[;#] ", line.strip())
@@ -109,13 +90,11 @@ def parse_line(line):
     char, name = match.groups()
     return {"name": name,"char": char, "code": code}
 
-
 def write_files(collected):
-    with open(os.path.join("emuji.json"), "w", encoding="utf-8") as f:
+    with open(os.path.join("emoji.json"), "w", encoding="utf-8") as f:
         json.dump(collected["full"], f, ensure_ascii=False, indent=2)
-    with open(os.path.join("emuji-only.json"), "w", encoding="utf-8") as f:
+    with open(os.path.join("emoji-only.json"), "w", encoding="utf-8") as f:
         json.dump(collected["compact"], f, ensure_ascii=False, indent=2)
-
 
 if __name__ == "__main__":
     main()
